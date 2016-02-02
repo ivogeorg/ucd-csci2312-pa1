47a0a28d1af05f2e4b71acdb3fa5b427182d1b64
Aaron Grissom <agrissom2@student.cccs.edu>
2016-02-02 10:35:02 -0700

    Changed point.h to contain 3 variables
    I Changed Point.h to contain 3 variables instead of 2

--- Point.h
+++ Point.h
@@ -1,4 +1,4 @@
-// A 2-dimensional point class!
+// A 3-dimensional point class!
 // Coordinates are double-precision floating point.
 
 #ifndef __POINT_H
@@ -7,20 +7,22 @@
 class Point {
     
 private:
-    double x, y;
+    double x, y, z;
 
 public:
     // Constructors
     Point();                      // default constructor
-    Point(double x, double y);    // two-argument constructor
+    Point(double x, double y, double z);    // three-argument constructor
 
     // Mutator methods
     void setX(double newX);
     void setY(double newY);
+	void setX(double newZ);
 
     // Accessor methods
     double getX() const;
     double getY() const;
+	double getZ() const;
     
 };
 
